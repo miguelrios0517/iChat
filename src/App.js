f69d96aeb4863828628e1b415477be8ce389152d
Miguel Rios <miguel.rios1997@gmail.com>
2021-02-15 21:39:27 -0800

    Add existing file

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header class = "header">
+        <div class = "logo"></div>
+        iChat
+      </header>
+    </div>
+  );
+}
+
+export default App;
