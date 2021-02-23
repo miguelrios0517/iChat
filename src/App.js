697c0c641074352f77e8df45fad4f4dad8251a5d
Miguel Rios <miguel.rios1997@gmail.com>
2021-02-22 17:49:52 -0800

    ready for styles

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import './App.css';
 import TextInput from './TextInput';
-
-import TextInput from './TextInput';
+import { useState } from 'react';
 
 function App() {
+  const [messages, setMessages] = useState([{text:'hello'}]);
   return (
     <div className="App">
       <header class = "header">
         <div class = "logo"></div>
         iChat
-      </header>
+      </header> 
+      <div className = "message">
+        {messages[0].text}
+      </div>
 
-      <TextInput/>
+      {/*This is a custom tag with a send function passed in as props*/}
+      <TextInput
+        send={(t)=> setMessages([{text:t}])}
+      />
       
     </div>
   );
--- src/TextInput.js
+++ src/TextInput.js
@@ -1,7 +1,33 @@
-function TextInput() {
-    return <footer className = "text-input">
-        Hello
+import {useState} from 'react';
+
+function TextInput(props) {
+    const [text, setText] = useState('') //may pass default value like useState('default value')
+    //text is the variable that is going to be set/updated
+    //setText is the function that sets the text
+    function sendMessage() {
+        if(text==='') return //skip the function 
+        props.send(text)
+        setText('')
+    }
     
+    function keyPressed(e) {
+        if(e.key==='Enter') {
+            sendMessage()
+        }
+
+    }
+
+
+    return <footer className = "text-input">
+        <input 
+            placeholder = "Write your message"
+            value = {text}
+            onChange={e=> setText(e.target.value)}
+            onKeyPress = {keyPressed}
+        />
+        <button onClick = {keyPressed} disabled = {!text}>
+            send
+        </button>
     </footer>
 
 }
